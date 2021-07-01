3ae6c0e3e9a0d86b0b9540393ac0e09f31b2a9b7
dragontww <adetariku@gmail.com>
2021-07-01 18:41:26 +0900

    Update faceDetectorMTCNN.py

--- faceDetectorMTCNN.py
+++ faceDetectorMTCNN.py
@@ -1,6 +1,6 @@
 """
  FaceDetection Based on MTCNN
- The program reds frames from a webcam, detects faces. Extract and saves cropped faces.
+ The program reads frames from a webcam, detects faces. Extract and saves cropped faces.
  
  It also displays a faces with bounding boxes 
 
